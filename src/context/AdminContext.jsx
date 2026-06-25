e0839ceda4449c7faf9a3a013f01141696ae3eb2
LucasAflores <41401194@fi.unju.edu.ar>
2026-06-24 21:44:22 -0300

    configuracion de carpetas y creacion del AdminContext

---
+++ src/context/AdminContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const AdminContext = createContext();
+
+export const AdminProvider = ({ children }) => {
+  
+  const [admin, setAdmin] = useState(() => {
+    const adminGuardado = localStorage.getItem('sesionAdmin');
+    return adminGuardado ? JSON.parse(adminGuardado) : null;
+  });
+
+  useEffect(() => {
+    if (admin) {
+      localStorage.setItem('sesionAdmin', JSON.stringify(admin));
+    } else {
+      localStorage.removeItem('sesionAdmin');
+    }
+  }, [admin]);
+
+  const login = (datosUsuario) => {
+    setAdmin(datosUsuario);
+  };
+
+  const logout = () => {
+    setAdmin(null);
+  };
+
+  return (
+    <AdminContext.Provider value={{ admin, login, logout }}>
+      {children}
+    </AdminContext.Provider>
+  );
+};
